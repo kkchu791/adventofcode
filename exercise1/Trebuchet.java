662bbc285210ac0b318514ef592d2dbbc3be31a1
kkchu791 <kkchu791@gmail.com>
2023-12-09 15:06:47 -0800

    adding first exercise

---
+++ exercise1/Trebuchet.java
@@ -0,0 +1,58 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Trebuchet {
+  public static void main(String[] args) {
+    String input_file_path="./part1_input.txt";
+
+    try {
+      BufferedReader reader = new BufferedReader(
+        new FileReader(input_file_path)
+      );
+
+      String line = reader.readLine();
+      int sum = 0;
+
+      while (line != null ){
+        System.out.println(line);
+        String digit = "";
+
+        for (int i = 0; i < line.length(); i++) {
+          if (Character.isDigit(line.charAt(i))) {
+            digit += line.charAt(i);
+            break;
+          }
+        }
+
+        for (int i = line.length() - 1; i >= 0; i--) {
+          if (Character.isDigit(line.charAt(i))) {
+            digit += line.charAt(i);
+            break;
+          }
+        }
+        System.out.println(digit);
+
+        sum += Integer.parseInt(digit);
+    
+        line = reader.readLine();
+      }
+
+      System.out.println(sum);
+    } catch(IOException error) {
+      error.printStackTrace();
+    }   
+  }
+}
+
+
+// read the file
+// read it line by line
+// iterate through the element
+// we need to grab the digits through two pointers
+// as we iterate, we'll add the digits
+// return the sum
+
+
+// present a design pattern
